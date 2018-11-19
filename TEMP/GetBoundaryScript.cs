5d85869fb31d6198042490fd73b7fe44e0aa6499
TiredPyro <33644150+TiredPyro@users.noreply.github.com>
2018-11-19 18:13:32 +0800

    bla

---
+++ TEMP/GetBoundaryScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBoundaryScript {
+
+    public static float ReturnCamVerticalBound(Camera gameCamera)
+    {
+        return gameCamera.orthographicSize;
+    }
+
+    public static float ReturnCamHorizontalBound(Camera gameCamera)
+    {
+        return gameCamera.orthographicSize * gameCamera.aspect;
+    }
+}
